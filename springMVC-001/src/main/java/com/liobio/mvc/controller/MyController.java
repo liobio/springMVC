c03c07b84088234319ed5b19d02d04d7a95c7be2
liobio <582437980@qq.com>
2021-08-06 21:55:05 +0800

    first commit

---
+++ springMVC-001/src/main/java/com/liobio/mvc/controller/MyController.java
@@ -0,0 +1,27 @@
+package com.liobio.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author LIOBIO
+ * @version 1.0.0
+ * @ClassName Mycontroller.java
+ * @createTime 2021/08/05/21:01:00
+ * @Description
+ */
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String index() {
+        //设置视图名称
+        return "index";
+    }
+    @RequestMapping("/target")
+    public String toTarget() {
+        return "target";
+    }
+
+
+}
