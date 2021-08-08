e77465177d41a1230f9d27e9c9e556d3b5686b0c
liobio <582437980@qq.com>
2021-08-08 13:54:38 +0800

    second

---
+++ springMVC-002/src/main/java/com/liobio/mvc/controller/ScopeController.java
@@ -0,0 +1,12 @@
+package com.liobio.mvc.controller;
+
+/**
+ * @author LIOBIO
+ * @version 1.0.0
+ * @ClassName ScopeController.java
+ * @createTime 2021/08/08/13:48:00
+ * @Description
+ */
+public class ScopeController {
+
+}
---
+++ springMVC-002/src/main/java/com/liobio/mvc/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.liobio.mvc.controller;
+
+/**
+ * @author LIOBIO
+ * @version 1.0.0
+ * @ClassName UserController.java
+ * @createTime 2021/08/08/13:48:00
+ * @Description
+ */
+public class UserController {
+}
