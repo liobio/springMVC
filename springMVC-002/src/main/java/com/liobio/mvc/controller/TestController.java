56ddb6e075260a79e390c10439143a8f4ecddbe9
liobio <582437980@qq.com>
2021-08-10 12:30:10 +0800

    view test

---
+++ springMVC-002/src/main/java/com/liobio/mvc/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.liobio.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author LIOBIO
+ * @version 1.0.0
+ * @ClassName TestController.java
+ * @createTime 2021/08/08/15:34:00
+ * @Description
+ */
+@Controller
+public class TestController {
+    @RequestMapping("/")
+    public String index(){
+        return "index" ;
+    }
+
+    @RequestMapping("/test_view")
+    public String testView(){
+        return "test_view";
+    }
+}
